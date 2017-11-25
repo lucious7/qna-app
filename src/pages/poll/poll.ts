f8c55b8eb6602443cb57c2be24adbc8d75158ef7
Renato <renattos@gmail.com>
2017-11-25 21:13:06 -0200

    add respondents selection in poll creation view

--- src/pages/poll/poll.ts
+++ src/pages/poll/poll.ts
@@ -11,11 +11,21 @@ export class PollPage {
   question: String = '';
   answers: String[] = []
   answer: String = null;
-  privacy: String = null;
-  transparency: String = null;
+  privacy: String = 'public';
+  transparency: String = 'opaque';
+  editability : String = 'immutable';
   open: boolean = false;
   replyOnce: boolean = false;
-  immutable: boolean = true;
+  respondents: Object[] = [];
+
+  friends: Object[] = [
+    {id: 1, name: 'Friend 1'},
+    {id: 2, name: 'Friend 2'},
+    {id: 3, name: 'Friend 3'},
+    {id: 4, name: 'Friend 4'},
+    {id: 5, name: 'Friend 5'},
+    {id: 6, name: 'Friend 6'},
+  ]
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
@@ -35,6 +45,17 @@ export class PollPage {
     var index = this.answers.indexOf(element, 0);
     if (index > -1) {
       this.answers.splice(index, 1);
+    }
+  }
+
+  addRespondent(respondent){
+    this.respondents.push(respondent);
+  }
+
+  removeRespondent(respondent){
+    var index = this.respondents.indexOf(respondent, 0);
+    if (index > -1) {
+      this.respondents.splice(index, 1);
     }
   }
 
