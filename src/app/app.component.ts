0f577c38fc89e86b81ea26efcf177765fd38a3de
Lucivaldo <lucious@MacBook-Pro-de-usuario.local>
2017-11-25 02:27:28 -0200

    handling side menu on logout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { Platform, NavController } from 'ionic-angular';
+import { MenuController } from 'ionic-angular/components/app/menu-controller';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { Subscription } from 'rxjs';
@@ -22,15 +23,19 @@ export class MyApp implements OnInit {
 
   rootPage: any = LoginPage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, private authService: AuthService) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, private authService: AuthService,
+              private menuCtrl: MenuController) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
 
+      this.enableMenu( false );      
+      
       this.authService.getAuth$().take(1).subscribe((user: firebase.User) => {
         if (!!user) {
           this.nav.setRoot(TabsPage).then(() => splashScreen.hide());
+          this.enableMenu( true );      
         } else {
           splashScreen.hide();
         }
@@ -45,12 +50,30 @@ export class MyApp implements OnInit {
   setAuthHandler(): Subscription {
     // skips the first authState change since it's done at the platform ready to handle splashscreen
     return this.authService.getAuth$().skip(1).subscribe((user: firebase.User) => {
-      this.nav.popToRoot().then(() => this.nav.setRoot(!!user ? TabsPage : LoginPage));
+      this.nav.popToRoot().then(() => {
+        if(!!user){
+          this.nav.setRoot(TabsPage);
+          this.enableMenu( true );        
+        } else {
+          this.nav.setRoot(LoginPage);
+          this.enableMenu( false );        
+        }
+      });
     });
   }
 
   logout() {
+    this.closeMenu();
+    this.enableMenu(false);
     this.authService.logout();
   }
 
+
+  private enableMenu(enable: boolean) {
+    this.menuCtrl.swipeEnable(enable);
+  }
+
+  private closeMenu() {
+    this.menuCtrl.close();
+  }
 }
