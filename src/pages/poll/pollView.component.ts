5fb09d20ee79340d3f09d40fe92a9f1a5785c943
Renato <renattos@gmail.com>
2017-11-26 01:05:41 -0200

    initial commit of poll viewer feature

---
+++ src/pages/poll/pollView.component.ts
@@ -0,0 +1,42 @@
+import { Component, Pipe } from '@angular/core';
+
+import { PipeTransform } from '@angular/core/src/change_detection/pipe_transform';
+import { NavController } from 'ionic-angular/navigation/nav-controller';
+import { NavParams } from 'ionic-angular/navigation/nav-params';
+
+
+@Component({
+  selector: 'app-pollview',
+  templateUrl: 'pollView.html',
+  pipes: [KeysPipe]
+})
+export class PollView {
+
+  question:Object = {
+    description: 'What is the best pizza made in somewhere?',
+    answers: {
+      A: 'Pizza A',
+      B: 'Pizza B',
+      C: 'Pizza C'
+    }
+  }
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  vote(event){
+    console.log("Vote");
+    this.dismiss(event);
+    
+  }
+
+  dismiss(event){
+    this.navCtrl.pop();
+  }
+}
+@Pipe({ name: 'KeysPipe',  pure: false })
+export class KeysPipe implements PipeTransform {
+    transform(value: any, args: any[] = null): any {
+        return Object.keys(value)//.map(key => value[key]);
+    }
+}
