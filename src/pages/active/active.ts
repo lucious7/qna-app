2dd27845900052d48dd41c2f4f40516f5810a372
fasfsfgs <fasfsfgs@gmail.com>
2017-11-26 20:56:29 -0200

    adding key to pollObjects in active tab
    Signed-off-by: fasfsfgs <fasfsfgs@gmail.com>

--- src/pages/active/active.ts
+++ src/pages/active/active.ts
@@ -17,14 +17,17 @@ import { Observable } from 'rxjs/Observable';
   templateUrl: 'active.html',
 })
 export class ActivePage {
+
   items: Observable<any[]>;
-  constructor(private afDB: AngularFireDatabase, private authService: AuthService) { 
-    this.items = this.afDB.list('respondents/' + this.authService.getUser().uid).valueChanges();
-    
+
+  constructor(private afDB: AngularFireDatabase, private authService: AuthService) {
+    this.items = this.afDB.list('respondents/' + this.authService.getUser().uid)
+      .snapshotChanges()
+      .map(pollActions => {
+        return pollActions.map(pollAction => ({ key: pollAction.key, ...pollAction.payload.val() }));
+      });
   }
 
-  ionViewDidLoad() {
-    
-  }
+  ionViewDidLoad() { }
 
 }
